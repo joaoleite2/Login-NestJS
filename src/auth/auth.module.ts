895db9d76d09fe98457907c0a1e0f76cde082299
joaoleite2 <joao.cont3t@gmail.com>
2023-12-12 16:55:17 -0300

    dependência circular.[fix]

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { UserModule } from "src/user/user.module";
@@ -9,10 +9,11 @@ import { AuthService } from "./auth.service";
     imports:[JwtModule.register({
         secret:")=|&W{.4?OI&-Kp+ZN_N4pyIxN%(ETwJ7V"
     }),
-    UserModule,
+    forwardRef(() => UserModule),
     PrismaModule,
     ],    
     controllers:[AuthController],
-    providers:[AuthService]
+    providers:[AuthService],
+    exports:[AuthService]
 })
 export class AuthModule{}
